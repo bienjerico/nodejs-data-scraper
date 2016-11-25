73a3b09f6c6e5883eeb27c0eb328bddbdc7ceae4
bienjerico <bien.jerico@gmail.com>
2016-11-25 12:27:08 +0800

    updates

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # NODEJS DATA SCRAPING
 * Install  *NodeJS*
-* Run *npm install*
+* Open Command Prompt 
+* Run *npm install* to install all dependencies
+* Run *node server.js* to start the api
+* Open Browser
+* Go to *http://localhost:3000/process-artists* to process the artists
+* Go to *http://localhost:3000/artists* to get the artist in html table
+
--- server.js
+++ server.js
@@ -5,17 +5,19 @@ var cheerio = require('cheerio');
 var app     = express();
 
 /* get GUITAR ARTIST */
-app.get('/', function (req, res) {
-		
+app.get('/process-artists', function (req, res) {
 
-	request('http://www.tcelectronic.com/artists/guitar-artists', function (error, response, body) {
-		
+	request('http://www.tcelectronic.com/artists/studio-pros', function (error, response, body) {
+
+		console.log("Start Data Scraping...");
+
 		if (!error && response.statusCode == 200) {
 			var $ = cheerio.load(body);
-			var list = $("#featured-guitar-artists");
+			var list = $("#featured-studio-pros");
 			var listDetails = list.find("ul li h3 a");
-			var obj = {guitarArtists: []};
+			var obj = {artists: []};
 			var cnt = 0;
+
 			listDetails.each(function(){
 
 				var artist = $(this).attr("href");
@@ -32,8 +34,7 @@ app.get('/', function (req, res) {
 								productdata += ',';
 							})
 								
-							console.log(productdata);
-							obj.guitarArtists.push({
+							obj.artists.push({
 											image : $(".artist-banner-image").attr('src'),
 											h2 : $("#variable-banner-text h2").text(),
 											h3 : $("#variable-banner-text h3").text(),
@@ -41,35 +42,34 @@ app.get('/', function (req, res) {
 											desc : $("#artist-description").children().text(),
 											products : productdata
 										}) 
-							fs.writeFile('data.json', JSON.stringify(obj, null, 4), 'utf-8');
+							fs.writeFile('studio-pros.json', JSON.stringify(obj, null, 4), 'utf-8');
 							cnt++;
-						  	console.log(cnt);
+						  	console.log(cnt +' - '+ $("#variable-banner-text h2").text());
 						}
-				  	})
-
-				
-			});
-
+			  		})
+			})
 		}
 	})
-
 })
 
 /* display GUITAR ARTIST */
-app.get('/guitar-artists',function(req,res){
-	fs.readFile('data.json', 'utf8', function (err, data) {
+app.get('/artists',function(req,res){
+	fs.readFile('studio-pros.json', 'utf8', function (err, data) {
+
 	    if (err) throw err; // we'll not consider error handling for now
+
 	    var obj = JSON.parse(data);
-	    var guitarArtist = obj.guitarArtists;
+	    var artists = obj.artists;
 	    var result = "<html><body><table table=1 cellpadding=0>";
 	    var cnt = 1;
-	    for(var x = 0 ; x < guitarArtist.length ; x++){
-			var image = guitarArtist[x].image; 
-			var name = guitarArtist[x].h2; 
-			var bandname = guitarArtist[x].h3; 
-			var message = guitarArtist[x].p; 
-			var desc = guitarArtist[x].desc; 
-			var products = guitarArtist[x].products; 
+
+	    for(var x = 0 ; x < artists.length ; x++){
+			var image = artists[x].image; 
+			var name = artists[x].h2; 
+			var bandname = artists[x].h3; 
+			var message = artists[x].p; 
+			var desc = artists[x].desc; 
+			var products = artists[x].products; 
 
 			result += "<tr><td>"+cnt+"</td><td>"+image+"</td><td>"+name+"</td><td>"+bandname+"</td><td>"+message+"</td><td>"+desc+"</td><td>"+products+"</td></tr>";
 			cnt++;
